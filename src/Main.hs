cf0149b985c621378ba07da701b5cbf1dc107cf2
Albert Yip <albertkaiyip@gmail.com>
2019-10-11 19:00:18 -0700

    Refactored jump logic

--- src/Main.hs
+++ src/Main.hs
@@ -9,41 +9,19 @@ module Main (main) where
 
     gameCycle :: GameAction ()
     gameCycle = do
-        (GameAttribute ga) <- getGameAttribute
+        (GameAttribute ga gb) <- getGameAttribute
         printOnScreen (show ga) TimesRoman24 (0,0) 1.0 1.0 1.0
-        dog <- findObject "dog" "dogGroup"
-        col1 <- objectLeftMapCollision dog
-        col2 <- objectRightMapCollision dog
-        when (col1 || col2) (reverseXSpeed dog)
-        col3 <- objectTopMapCollision dog
-        when col3 (reverseYSpeed dog)
-        col4 <- objectBottomMapCollision dog
-        when col4 (funExit)
-
-    moveDogLeft :: Modifiers -> Position -> GameAction ()
-    moveDogLeft m p = do
-        obj <- findObject "dog" "dogGroup"
-        (pX, pY) <- getObjectPosition obj
-        (sX, _) <- getObjectSize obj
-        if (pX - (sX/2) - 5 >= 0)
-            then (setObjectPosition ((pX - 5),pY) obj)
-            else (setObjectPosition (sX/2,pY) obj)
-
-
-    moveDogRight :: Modifiers -> Position -> GameAction ()
-    moveDogRight m p = do
-        obj <- findObject "dog" "dogGroup"
-        (pX, pY) <- getObjectPosition obj
-        (sX, _) <- getObjectSize obj
-        if (pX + (sX/2) + 5 <= 250)
-            then (setObjectPosition ((pX + 5),pY) obj)
-            else (setObjectPosition ((250 - (sX/2)),pY) obj)
+        dogCycle gb
+        -- col1 <- objectLeftMapCollision dog --
+        -- col2 <- objectRightMapCollision dog --
+        -- when (col1 || col2) (reverseXSpeed dog) --
+        -- col3 <- objectTopMapCollision dog --
+        -- when col3 (reverseYSpeed dog) --
         
     main :: IO ()
     main = let winConfig = ((0, 0), windowSize, "hachi")
                gameMap = textureMap 4 1920 1200 1920.0 1200.0
                dogGroup = objectGroup "dogGroup" [dog]
-               initAttr = GameAttribute 5
-               input = [(SpecialKey KeyLeft, StillDown, moveDogLeft),
-                    (SpecialKey KeyRight, StillDown, moveDogRight)]
+               initAttr = GameAttribute 0 False
+               input = [(MouseButton LeftButton, Press, jump)]
             in funInit winConfig gameMap [dogGroup] () initAttr input gameCycle (Timer 40) pictures
--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,12 @@
-module Types where
+module Types(
+    dogCycle,
+    jump,
+    GameAction,
+    GameAttribute(GameAttribute),
+    Dog,
+    dog,
+    windowSize
+) where
 
     import Graphics.UI.Fungen
     
@@ -6,7 +14,7 @@ module Types where
 
     type Dog = GameObject ()
 
-    data GameAttribute = GameAttribute Int
+    data GameAttribute = GameAttribute Int Bool
 
     type GameAction a = IOGame GameAttribute () () () a
 
@@ -14,10 +22,19 @@ module Types where
     windowSize = (1920, 1200)
 
     dogSize :: (Double, Double)
-    dogSize = (47, 67)
+    dogSize = (192, 192)
+
+    dogSpeed :: Double
+    dogSpeed = 20.0
+
+    gravity :: Double
+    gravity = 1.0
+
+    maxHeight :: Double
+    maxHeight = fromIntegral (snd windowSize `div` 4)
 
     startPos :: (Double, Double)
-    startPos = (fromIntegral (fst windowSize `div` 2), fromIntegral (snd windowSize `div` 8))
+    startPos = (fromIntegral (fst windowSize `div` 4), fromIntegral (snd windowSize `div` 8))
 
     initDogPicture :: (Double, Double) -> Int -> ObjectPicture
     initDogPicture (x, y) = Tex (x, y)
@@ -27,3 +44,51 @@ module Types where
 
     dog :: Dog
     dog = initDog "dog" (initDogPicture dogSize 0)  False startPos (0, 0)
+
+
+    dogCycle :: Bool -> GameAction ()
+    dogCycle isJump = do
+        dog <- findObject "dog" "dogGroup"
+        (GameAttribute score isJump) <- getGameAttribute
+        (_, vY) <- getObjectSpeed dog
+        when (isJump) (handleMotion dog vY)
+
+    handleMotion :: Dog -> Double -> GameAction ()
+    handleMotion dog vY
+            | vY > 0 = jumping dog
+            | vY < 0 = falling dog
+                     
+
+    jumping :: Dog -> GameAction ()
+    jumping dog = do
+        (vX, vY) <- getObjectSpeed dog
+        (_, pY) <- getObjectPosition dog
+        if (pY >= maxHeight)
+            then reverseYSpeed dog
+            else setObjectSpeed (vX, newSpeed vY) dog
+            where newSpeed vY
+                    | vY - gravity > 0 = vY - gravity
+                    | otherwise = vY
+
+
+    falling :: Dog -> GameAction ()
+    falling dog = do
+        (vX, vY) <- getObjectSpeed dog
+        (_, pY) <- getObjectPosition dog
+        if (pY <= snd startPos)
+            then stop dog vX
+            else setObjectSpeed (vX, vY - gravity) dog
+
+    jump :: Modifiers -> Position -> GameAction ()
+    jump _ _ = do
+        (GameAttribute score _) <- getGameAttribute
+        setGameAttribute (GameAttribute score True)
+        dog <- findObject "dog" "dogGroup"
+        (vX, vY) <- getObjectSpeed dog
+        when (vY <= 0) (setObjectSpeed (vX, dogSpeed) dog)
+
+    stop :: Dog -> Double -> GameAction ()
+    stop dog vX = do
+        (GameAttribute score _) <- getGameAttribute
+        setGameAttribute (GameAttribute score False)
+        setObjectSpeed (vX, 0) dog
