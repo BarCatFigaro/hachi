290ce4fdaf4e7d1a273934df483d8ae4eb67dd3c
Christine Song <47646795+ongsemble@users.noreply.github.com>
2019-10-22 23:16:50 -0700

    Added documentation (#19)
    * Cropped dog tiles
    
    * Added working obstacles
    
    * Fixed dog tiles with colour replacement
    
    * Fixed dog tiles again
    
    * Touch-up of dog files
    
    * Another touch-up of dog tiles
    
    * Added obstacles and powerup sprites
    
    * Added documentation

--- src/Dog.hs
+++ src/Dog.hs
@@ -6,27 +6,40 @@ module Dog where
     import Item
     import Types
 
+    -- dogSpeed speed of the dog
     dogSpeed :: Double
     dogSpeed = 40.0
 
+    -- dogSize size of the dog
     dogSize :: (Double, Double)
     dogSize = (201, 195)
 
+    -- gravity the speed a jumping dog falls back to the ground every cycle
     gravity :: Double
     gravity = 0.1
 
+    -- extraJumpHeight the maximum height a jumping dog can reach on the screen
     extraJumpHeight :: Double
     extraJumpHeight = fromIntegral $ snd windowSize `div` 16
 
+    -- startPos the starting position of the dog on the screen
     startPos :: (Double, Double)
     startPos = (fromIntegral $ fst windowSize `div` 4, fromIntegral $ snd windowSize `div` 8)
 
+    -- initDog represents the starting dog
+    -- name name of dog object
+    -- pic dog sprite
+    -- isAsleep true if dog is asleep; false otherwise
+    -- pos x,y-position of the dog
+    -- speed x velocity and y velocity of the dog
     initDog :: String -> ObjectPicture -> Bool -> (Double, Double) -> (Double, Double) -> Dog
     initDog name pic isAsleep pos speed = object name pic isAsleep pos speed ()
 
+    -- createDog creates a new dog
     createDog :: Dog
     createDog = initDog "dog" (initPicture dogSize 0)  False startPos (0, 0)
 
+    -- dogCycle handles all actions in a cycle of the game
     dogCycle :: GameAction ()
     dogCycle = do
         dog <- findObject "dog" "dogGroup"
@@ -42,6 +55,8 @@ module Dog where
         when isJump $ handleMotion dog vY
         handleCollision items
 
+    -- handleCollision handles the collisions between a dog and an item
+    -- (x:xs) the list of items (obstacles, powerups) in the game
     handleCollision :: [Item] -> GameAction ()
     handleCollision [] = return ()
     handleCollision (x:xs) = do
@@ -50,6 +65,10 @@ module Dog where
         hasCollided <- objectsCollision x dog
         when (hasCollided) (handleCollisionHelper dog obsName x)
 
+    -- handleCollisionHelper determines how to handle a collision, distinguishing between powerup and obstacle
+    -- dog the dog in the game
+    -- name the name of the item currently colliding with the dog
+    -- item represents the item colliding with the dog
     handleCollisionHelper :: Dog -> String -> Item -> GameAction ()
     handleCollisionHelper dog name item = do
         (GameAttribute _ _ _ (PowerUpInfo hasHit powerUp _)) <- getGameAttribute
@@ -68,23 +87,33 @@ module Dog where
                 addObjectsToGroup [newItem] "itemGroup"                
                 (GameAttribute score isJump dogState (PowerUpInfo _ powerUp duration)) <- getGameAttribute
                 setGameAttribute (GameAttribute score isJump dogState (PowerUpInfo True powerUp duration))
-                 
+    
+    -- handlePowerUp updates the powerup for active state and duration left
+    -- dog the dog in the game
+    -- powerUp current powerup available in the game
     handlePowerUp :: Dog -> PowerUp -> GameAction ()
     handlePowerUp dog powerUp = do
         (GameAttribute score isJump dogState (PowerUpInfo _ powerUp duration)) <- getGameAttribute
         setGameAttribute (GameAttribute score isJump dogState (PowerUpInfo (duration > 0) powerUp (duration - 1)))
 
+    -- stopMovingObs stops all items in the game
+    -- (x:xs) list of all items in the game
     stopMovingObs :: [Item] -> GameAction ()
     stopMovingObs [] = return ()
     stopMovingObs (x:xs) = do
         setObjectSpeed (0, 0) x
         stopMovingObs xs
 
+    -- handleMotion handles the jumping and falling of the dog
+    -- dog the dog in the game
+    -- vY y velocity of the dog
     handleMotion :: Dog -> Double -> GameAction ()
     handleMotion dog vY
             | vY > 0 = jumping dog
             | vY < 0 = falling dog
-                     
+               
+    -- jumping handles the jumping motion of the dog
+    -- dog the dog in the game
     jumping :: Dog -> GameAction ()
     jumping dog = do
         (vX, vY) <- getObjectSpeed dog
@@ -101,6 +130,8 @@ module Dog where
                     | vY - gravity > 0 = vY - gravity
                     | otherwise = vY
 
+    -- falling handles the falling motion of the dog
+    -- dog the dog in the game
     falling :: Dog -> GameAction ()
     falling dog = do
         (vX, vY) <- getObjectSpeed dog
@@ -109,6 +140,7 @@ module Dog where
             then stop dog vX
             else setObjectSpeed (vX, vY - gravity) dog
 
+    -- jump updates the dog's state of jumping or falling and the effects of gravity on the motion
     jump :: GameAction ()
     jump = do
         (GameAttribute score _ dogState powerUpInfo) <- getGameAttribute
@@ -120,6 +152,9 @@ module Dog where
                 | vY == 0 = dogSpeed
                 | otherwise = vY
 
+    -- stop stops the dog
+    -- dog the dog in the game
+    -- vX x velocity of the dog
     stop :: Dog -> Double -> GameAction ()
     stop dog vX = do
         (GameAttribute score _ dogState powerUpInfo) <- getGameAttribute
--- src/Types.hs
+++ src/Types.hs
@@ -4,21 +4,23 @@ module Types where
     
     import Graphics
 
+    -- Dog is a FunGen game object, representing the dog
     type Dog = GameObject ()
 
+    -- Item is a FunGen game object, representing the obstacles and powerups
     type Item = GameObject ()
 
     {- GameAttribute is one of:
         - score: Int, current score of the current game session
         - isJump: Bool, whether we are currently in the midst of jumping
         - dogState: Int, which dog state (in terms of sprite) we are in
-        - powerUpInfo: PowerUpInfo
+        - powerUpInfo: PowerUpInfo, stores the current powerup
     -}
     data GameAttribute = GameAttribute Int Bool Int PowerUpInfo
 
     {- PowerUpInfo consists of:
         - Bool: whether we have hit a power up
-        - PowerUp: the power up
+        - PowerUp: the type of power up
         - Int: duration of the power up
      -}
     data PowerUpInfo = PowerUpInfo Bool PowerUp Int
@@ -30,27 +32,38 @@ module Types where
     -}
     data PowerUp = NoPower | JumpPower | NoClipPower deriving Eq
 
+    -- GameState represents the continuation or game over of the current game
     data GameState = GameOver | GameCont
 
+    -- GameAction contains the game attributes and game state of the current game
     type GameAction a = IOGame GameAttribute () GameState () a
 
+    -- windowSizeX is the Int width of the game window
     windowSizeX :: Int
     windowSizeX = 1920
 
+    -- windowSizeY is the Int height of the game window
     windowSizeY :: Int
     windowSizeY = 1200
 
+    -- mapWidth is the Double width of the game window
     mapWidth :: Double
     mapWidth = fromIntegral windowSizeX
 
+    -- mapHeight is the Double height of the game window
     mapHeight :: Double
     mapHeight = fromIntegral windowSizeY
 
+    -- windowSize is the window size of the current game
     windowSize :: (Int, Int)
     windowSize = (windowSizeX, windowSizeY)
 
+    -- maxHeight is the maximum height permitted for all initialized items and dogs
     maxHeight :: Double
     maxHeight = fromIntegral $ snd windowSize `div` 4
 
+    -- initPicture represents the picture of an object
+    -- x the size of the picture
+    -- y the index of the picture in pictures (from Graphics.hs)
     initPicture :: (Double, Double) -> Int -> ObjectPicture
-    initPicture (x, y) = Tex (x, y)
+    initPicture x y = Tex (x, y)
